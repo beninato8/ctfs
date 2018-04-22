df724264da13d205ec0f8c0deb12f82abd2b33f5
beninato8 <beninato8@gmail.com>
2018-04-22 11:54:07 -0700

    decrypt a file that has been encrypted multiple times

---
+++ pactf/round-1/Siblings/decrypt.sh
@@ -0,0 +1,9 @@
+cp unbreakable_code d20
+for i in {19..1}; do 
+    # echo "**"
+    # echo $i;
+    # echo $(($i-1));
+    openssl rsautl -decrypt -inkey privatekey$i.pem -in d$(($i+1)) -raw -out d$i
+done
+openssl rsautl -decrypt -inkey privatekey0.pem -in d1 -raw -out flag
+cat flag
